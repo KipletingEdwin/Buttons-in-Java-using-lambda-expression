1aa025369dfe6bcbbf6b87de504b5940870f0a68
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-02 08:54:11 +0300

    add an instance of JFrame class

---
+++ src/MyFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    JButton button;
+
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+        this.setVisible(true);
+
+    }
+
+}
