76a5b7f37264b38f4fd3caf09505e2ddca51f7ab
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-02 09:20:45 +0300

    setFont of the text in the image button

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,15 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
     JButton button;
 
 
     MyFrame(){
+        ImageIcon icon = new ImageIcon("thumb.jpeg");
+
 
         button = new JButton();
-        button.setBounds(200,100,150,50);
+        button.setBounds(180,100,150,50);
         button.addActionListener(e -> System.out.println("Hey bro, you are a genious"));
         button.setText("I'm a button!");
+        button.setFocusable(false);
+        button.setIcon(icon);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setFont(new Font("Comic Sans",Font.BOLD,25));
 
 
 
