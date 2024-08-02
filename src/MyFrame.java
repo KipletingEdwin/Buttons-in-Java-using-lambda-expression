2dd2c7283828f378fc2781642192a2590cca6965
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-02 09:02:09 +0300

    add text to the button

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -5,11 +5,21 @@ public class MyFrame extends JFrame {
 
 
     MyFrame(){
+
+        button = new JButton();
+        button.setBounds(200,100,150,50);
+        button.addActionListener(e -> System.out.println("Hey bro, you are a genious"));
+        button.setText("I'm a button!");
+
+
+
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLayout(null);
         this.setVisible(true);
 
+        this.add(button);
     }
 
 }
